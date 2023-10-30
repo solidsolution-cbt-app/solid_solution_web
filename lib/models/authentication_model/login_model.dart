d202dc22de44364b17879740319ff90bc8a2c648
dewunmirokeeb <adewunmirokeeb6@gmail.com>
2023-10-30 01:36:45 +0100

    fix security issue

--- lib/models/authentication_model/login_model.dart
+++ lib/models/authentication_model/login_model.dart
@@ -26,18 +26,3 @@ class LoginModel {
     return data.toString();
   }
 }
-
-
-
-/*
-
-{
-    "success": true,
-    "message": "Admin logged in successfully",
-    "data": {
-        "phoneVerified": true,
-        "auth": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MWFhOTQzNzVlMDliMmU2ZTBlYTIxNCIsImNyZWF0ZWRBdCI6IjIwMjMtMTAtMDJUMTE6Mjg6MDMuNDEwWiIsInVwZGF0ZWRBdCI6IjIwMjMtMTAtMDJUMTE6Mjg6MjIuMjg3WiIsImZ1bGxOYW1lIjoiTGF3YWwgWXVzdWYiLCJ1c2VyTmFtZSI6InNvbGlkc29sdXRpb25zMDAxIiwicGFzc3dvcmQiOiIkMmEkMTAkQWlOSnVubG0uaFp0a3kxemJrdWM4LmhKZDZVWnBwNmJvbVFQeWtEeVhneVh1T0p3ajlPL0MiLCJlbWFpbCI6Im51cnVkZWVucmFiaXU4MUBnbWFpbC5jb20iLCJwaG9uZU51bWJlciI6IjA5MDM3NTgzNDMwIiwiYWNjb3VudFN0YXR1cyI6ImFjdGl2ZSIsInBob25lVmVyaWZpY2F0aW9uVG9rZW4iOiIiLCJwaG9uZVZlcmlmaWNhdGlvblRva2VuVFRMIjoiIiwiZW1haWxWZXJpZmljYXRpb25Ub2tlbiI6IiIsImVtYWlsVmVyaWZpZWQiOnRydWUsInBob25lVmVyaWZpZWQiOnRydWUsInBhc3N3b3JkUmVzZXRUb2tlblRUTCI6IiIsInBhc3N3b3JkUmVzZXRUb2tlbiI6IiIsImF2YXRhciI6IiIsImxvZ2luU2Vzc2lvbiI6InhwTXl5eUNOYVRlMWZTM0FhQ2hsZyIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTY5NjI0NjEwMiwiZXhwIjoxNjk4ODM4MTAyfQ.w0Ioyqq7bwficfcePccnUhaS3jYzqXLYJpj40hTwZ2Y"
-    }
-}
-
- */
